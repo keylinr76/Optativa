81087df483ac60518d8c4209f8e1a491c30cac10
Estudiante <Estudiante@lab.ing.edu.ni>
2024-05-18 19:34:30 +0200

    optativa

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Multiplexación por División en Frecuencia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero de usuarios:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(74, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ancho de banda por canal:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(67, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(231, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ancho de banda de guarda:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(58, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Frecuencia central del canal:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(342, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(42, 391);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(723, 205);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(354, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(354, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(354, 237);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(354, 295);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(129, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 620);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int numeroUsuarios) || numeroUsuarios <= 0)
+            {
+                MessageBox.Show("Ingrese un número válido de usuarios.");
+                textBox1.Focus();
+                return;
+            }
+
+            if (!float.TryParse(textBox2.Text, out float anchoBandaCanal) || anchoBandaCanal <= 0)
+            {
+                MessageBox.Show("Ingrese un ancho de banda válido para el canal.");
+                textBox2.Focus();
+                return;
+            }
+
+            if (!float.TryParse(textBox3.Text, out float anchoBandaGuarda) || anchoBandaGuarda <= 0)
+            {
+                MessageBox.Show("Ingrese un ancho de banda válido para la guarda.");
+                textBox3.Focus();  
+                return;
+            }
+
+            if (!float.TryParse(textBox4.Text, out float frecuenciaCanal) || frecuenciaCanal <= 0)
+            {
+                MessageBox.Show("Ingrese una frecuencia válida para el canal.");
+                textBox4.Focus();
+                return;
+            }
+
+            DatosMultiplexacion datos = new DatosMultiplexacion
+            {
+                NumeroUsuarios = numeroUsuarios,
+                AnchoBandaCanal = anchoBandaCanal,
+                AnchoBandaGuarda = anchoBandaGuarda,
+                FrecuenciaCentralCanal1 = frecuenciaCanal
+            };
+
+            // Calcular resultados
+            float anchoBandaTotal = CalcularAnchoBandaTotal(datos);
+            float tasaKbpsTotal = CalcularTasaKbpsTotal(datos);
+            float tasaKbpsTotalSinGuarda = CalcularTasaKbpsTotalSinGuarda(anchoBandaTotal);
+
+            // Mostrar resultados
+            richTextBox1.Clear();
+            richTextBox1.AppendText("Datos ingresados:\n");
+            richTextBox1.AppendText($"Número de usuarios: {datos.NumeroUsuarios}\n");
+            richTextBox1.AppendText($"Ancho de banda por canal: {datos.AnchoBandaCanal} kHz\n");
+            richTextBox1.AppendText($"Ancho de banda de guarda: {datos.AnchoBandaGuarda} kHz\n");
+            richTextBox1.AppendText($"Frecuencia del canal: {datos.FrecuenciaCentralCanal1} MHz\n\n");
+
+            for (int i = 1; i <= datos.NumeroUsuarios; i++)
+            {
+                float frecuenciaPortadoraUsuarioActual = CalcularFrecuenciaPortadoraUsuario(datos, i);
+                richTextBox1.AppendText($"Frecuencia portadora del usuario {i}: {frecuenciaPortadoraUsuarioActual:F6} MHz\n");
+            }
+
+            richTextBox1.AppendText($"\nAncho de banda total: {anchoBandaTotal:F2} kHz\n");
+            richTextBox1.AppendText($"Número total de Kbps que se transmiten: {tasaKbpsTotal:F2} kbps\n");
+            richTextBox1.AppendText($"Número total de Kbps que podrían transmitirse utilizando todo el ancho de banda: {tasaKbpsTotalSinGuarda:F2} kbps\n");
+            LimpiarFormulario();
+        }
+        private float CalcularFrecuenciaPortadoraUsuario(DatosMultiplexacion datos, int numeroUsuario)
+        {
+            return datos.FrecuenciaCentralCanal1 + (numeroUsuario - 1) * (datos.AnchoBandaCanal + datos.AnchoBandaGuarda) / 1000.0f;
+        }
+
+        private float CalcularAnchoBandaTotal(DatosMultiplexacion datos)
+        {
+            return 2 * (datos.AnchoBandaCanal / 2) + (datos.NumeroUsuarios - 1) * (datos.AnchoBandaCanal + datos.AnchoBandaGuarda);
+        }
+
+        private float CalcularTasaKbpsUsuario()
+        {
+            return 100.0f; // Tasa fija de transmisión por usuario en kbps
+        }
+
+        private float CalcularTasaKbpsTotal(DatosMultiplexacion datos)
+        {
+            return datos.NumeroUsuarios * CalcularTasaKbpsUsuario();
+        }
+
+        private float CalcularTasaKbpsTotalSinGuarda(float anchoBandaTotal)
+        {
+            return anchoBandaTotal * 0.5f; // Utilizando toda la banda, relación 0.5 bps/Hz
+        }
+        private void LimpiarFormulario()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox1.Focus(); 
+        }
+
+        public struct DatosMultiplexacion
+        {
+            public int NumeroUsuarios;
+            public float AnchoBandaCanal;
+            public float AnchoBandaGuarda;
+            public float FrecuenciaCentralCanal1;
+        }
+    }
+}
